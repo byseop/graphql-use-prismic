38bc8be46b8b63cee8b968fc4a86a0077258d69c
Jun-Cheol Park <pakjce@omnious.com>
2020-11-25 00:33:46 +0900

    webhook handler 작업 시작

---
+++ webhook-handler/handler.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// const fetch = require('node-fetch');
+
+module.exports.webhookForDev = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+
+module.exports.webhookForProd = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
