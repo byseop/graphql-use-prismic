d6d7834c98fcb5eaf1060015636e5be79a94ef88
Youngseop Bae <byseop@gmail.com>
2020-12-03 18:37:54 +0900

    test build

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
-if (rootElement?.hasChildNodes()) {
-  hydrate(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement
-  );
-} else {
-  render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    rootElement
-  );
-}
+// if (rootElement?.hasChildNodes()) {
+hydrate(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  rootElement
+);
+// }
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
