4773db3941cbac1e817b891332147f8b4a6bfce0
Youngseop Bae <byseop@gmail.com>
2020-12-03 18:41:49 +0900

    Revert "test build"
    This reverts commit d6d7834c98fcb5eaf1060015636e5be79a94ef88.

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,21 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
-// if (rootElement?.hasChildNodes()) {
-hydrate(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
-// }
+if (rootElement?.hasChildNodes()) {
+  hydrate(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+} else {
+  render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
